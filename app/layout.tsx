80f13011be478adab1d40a421edc975e1fef68c7
Kafazevc <kafazevc@gmail.com>
2024-04-26 16:46:44 -0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import { AldrichFont } from './ui/fonts';
+import './ui/global.css'
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${AldrichFont.className} antialiased`}>
       <body>{children}</body>
     </html>
   );
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import {Aldrich, Lusitana} from 'next/font/google'
+
+
+
+export const AldrichFont = Aldrich( { subsets: ['latin'], weight: '400' } )
+export const lusitana = Lusitana( { subsets: ['latin'], weight: '400' } )
